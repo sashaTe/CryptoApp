cdcff94f6de81c1dd8c72972a79fa0e179d5c638
Alexander Zarutskiy <sashasolo23@gmail.com>
2023-06-18 16:27:27 +0100

    Initial Commit

---
+++ CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by Alexander Zarutskiy on 18.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
